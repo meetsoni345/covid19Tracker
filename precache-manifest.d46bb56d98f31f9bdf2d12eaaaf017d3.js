424fde252c9f8631b18ba837a855b066c96b1542
Meet Soni <meet.soni@emxcelsolutions.com>
2020-07-10 20:33:54 +0530

    Updates

---
+++ precache-manifest.d46bb56d98f31f9bdf2d12eaaaf017d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df70a49569380bbdb41d9e5800191610",
+    "url": "/covid19Tracker/index.html"
+  },
+  {
+    "revision": "dc75ff120dfada1a5101",
+    "url": "/covid19Tracker/static/css/main.7e19aa38.chunk.css"
+  },
+  {
+    "revision": "7911e5b67334dc92877d",
+    "url": "/covid19Tracker/static/js/2.3179fd3e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19Tracker/static/js/2.3179fd3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc75ff120dfada1a5101",
+    "url": "/covid19Tracker/static/js/main.03b5e6a1.chunk.js"
+  },
+  {
+    "revision": "fdb64d02364852e94798",
+    "url": "/covid19Tracker/static/js/runtime-main.5e965be9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19Tracker/static/media/image.d7265326.png"
+  }
+]);
